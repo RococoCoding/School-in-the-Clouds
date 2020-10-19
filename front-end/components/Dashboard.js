79c189d131bacd1635ab6d6167c3976de2f5d862
Simone Ballard <simone.a.ballard@gmail.com>
2020-10-18 20:40:40 -0600

    starter code added for dashboard

---
+++ front-end/components/Dashboard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Admin } from './Admin';
+import { Student } from './Student';
+import { Volunteer } from './Volunteer';
+
+export const Dashboard = () => {
+    const admin = useSelector(state => state.landingReducer.admin);
+    const student = useSelector(state => state.landingReducer.student);
+    const volunteer = useSelector(state => state.landingReducer.volunteer);
+
+    return (
+        <>
+            {
+                admin ? <Admin /> : null
+            }
+
+            {
+                student ? <Student /> : null
+            }
+
+            {
+                volunteer ? <Volunteer /> : null
+            }
+        </>
+    )
+}
